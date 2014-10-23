44662c43a8fcb18c2888a020e6ba0b4f1338d3e2
andrea <andrea@debian>
2014-10-24 00:55:58 +0200

    map range error

--- about_associative_arrays.d
+++ about_associative_arrays.d
@@ -6,13 +6,39 @@ import dunit;
 class AboutAssociativeArrays
 {
 	mixin UnitTest;
-    
+/*    
    @Test 
     void creation_and_assignment() {
       int [string] mymap;
       mymap["hello"]=3;
       assertEquals(mymap["hello"],FILL_IN_THIS_NUMBER);
   }
+
+*/
+
+   @Test
+     void other_operations() {
+       int [string] ages = ["bob":20, "joe":30, "dan":40];
+
+       assertEquals(ages.length, FILL_IN_THIS_NUMBER);
+
+       auto age=ages["bob"];
+       assertEquals(age, FILL_IN_THIS_NUMBER);
+
+       import core.exception: RangeError;
+       try 
+       {
+         auto age=ages["steven"];
+       }
+       catch (RangeError e)
+       {
+         // accessing non existing elements raise an exception
+         assertEquals(e.msg,FILL_IN_THIS_STRING);
+       }
+
+
+
+     }
   
   
 }
