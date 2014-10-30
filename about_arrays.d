55ddda3a1bd386737e9122ad0e7abf8996f0bb8f
Andrea Manzini <ilmanzo@gmail.com>
2014-10-30 23:03:47 +0100

    arrays

--- about_arrays.d
+++ about_arrays.d
@@ -6,12 +6,24 @@ import dunit;
 class AboutArrays
 {
 	mixin UnitTest;
-    
-   @Test 
-    void creation_and_assignment() {
-
-
+	
+   
+  @Test
+   void fixed_length_arrays() {
+     string[4] fruits=["banana","mango","apple","orange"];
+	  assertEquals(fruits[0],FILL_IN_THIS_STRING);
+	  assertEquals(fruits.length,FILL_IN_THIS_NUMBER);
   }
+
+   @Test 
+    void dynamic_length_arrays() {
+      string[] fruits=["banana","mango"];
+	  assertEquals(fruits.length,FILL_IN_THIS_NUMBER);
+	  fruits~="strawberry";
+	  assertEquals(fruits.length,FILL_IN_THIS_NUMBER);
+	  assertEquals(fruits[2],FILL_IN_THIS_STRING);
+  }
+
   
   
 }
