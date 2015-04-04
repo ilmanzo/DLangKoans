9d8b7b907533397fe716c88a238674fa6bbe1c21
nerdinand <nerdinand@nerdinand.com>
2015-04-04 15:02:04 +0200

    improve code styling in about_strings.d

--- source/about_strings.d
+++ source/about_strings.d
@@ -3,48 +3,42 @@ module about_strings;
 import helpers;
 import dunit;
 
-class AboutStrings
-{
-    mixin UnitTest;
-	
-	@Test
-	public void stringSlicing()
-	{
-	  auto sentence="The quick brown fox";
-	  assertEquals(sentence[0],FILL_IN_THIS_CHAR);
-	  assertEquals(sentence[1..3],FILL_IN_THIS_STRING);
-	  assertEquals(sentence[$],FILL_IN_THIS_CHAR);
-	  assertEquals(sentence[$-3..$],FILL_IN_THIS_STRING);
-	}
+class AboutStrings {
+  mixin UnitTest;
 
   @Test
-  public void stringConcatenation()
-  {
-    auto str1="Hello";
-    auto str2=" World";
-    auto str=str1 ~ str2;
+  public void stringSlicing() {
+    auto sentence = "The quick brown fox";
+    assertEquals(sentence[0], FILL_IN_THIS_CHAR);
+    assertEquals(sentence[1..3], FILL_IN_THIS_STRING);
+    assertEquals(sentence[$], FILL_IN_THIS_CHAR);
+    assertEquals(sentence[$-3..$], FILL_IN_THIS_STRING);
+  }
+
+  @Test
+  public void stringConcatenation() {
+    auto str1 = "Hello";
+    auto str2 = " World";
+    auto str = str1 ~ str2;
     assertEquals(str, FILL_IN_THIS_STRING);
   }
 
   @Test
-    public void stringsAreImmutable()
-    {
-      auto str = "hello";
-      char[] s = "hello".dup;
-      s[0] = 'H';
-      assertEquals(s,FILL_IN_THIS_STRING);
-    }
+  public void stringsAreImmutable() {
+    auto str = "hello";
+    char[] s = "hello".dup;
+    s[0] = 'H';
+    assertEquals(s, FILL_IN_THIS_STRING);
+  }
 
   @Test
-    public void unicodeLiterals()
-    {
-      string s = "résumé"c;   // same as "résumé"
-      wstring w = "résumé"w;
-      dstring d = "résumé"d;
+  public void unicodeLiterals() {
+    string s = "résumé"c;   // same as "résumé"
+    wstring w = "résumé"w;
+    dstring d = "résumé"d;
 
-      assertEquals(s.length,FILL_IN_THIS_NUMBER);
-      assertEquals(w.length,FILL_IN_THIS_NUMBER);
-      assertEquals(d.length,FILL_IN_THIS_NUMBER);
-    }
-
+    assertEquals(s.length, FILL_IN_THIS_NUMBER);
+    assertEquals(w.length, FILL_IN_THIS_NUMBER);
+    assertEquals(d.length, FILL_IN_THIS_NUMBER);
+  }
 }
