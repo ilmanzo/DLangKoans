75631905503bb4f5a5780da49c93331e96a8944a
nerdinand <nerdinand@nerdinand.com>
2015-04-04 15:08:34 +0200

    improve code styling in about_templates.d

--- source/about_templates.d
+++ source/about_templates.d
@@ -3,28 +3,23 @@ module about_templates;
 import helpers;
 import dunit;
 
-class AboutTemplates
-{
-    mixin UnitTest;
+class AboutTemplates {
+  mixin UnitTest;
 
-    T twice(T)(T value)  //shortcut syntax
-    {
-        return 2*value;
-    }
+  T twice(T)(T value){  //shortcut syntax
+      return 2 * value;
+  }
 
-    T twice(T : string)(T value)  // specialization
-    {
-        return value~value; 
-    }
-	
-	@Test
-	public void functionTemplates()
-	{
-	  auto val1=twice!int(2);  // instantiation
-	  assertEquals(val1, FILL_IN_THIS_NUMBER);
-	  auto val2=twice("hello");
-	  assertEquals(val2, FILL_IN_THIS_STRING);
-	}
+  T twice(T : string)(T value){ // specialization
+    return value ~ value;
+  }
 
-
+  @Test
+  public void functionTemplates()
+  {
+    auto val1 = twice!int(2);  // instantiation
+    assertEquals(val1, FILL_IN_THIS_NUMBER);
+    auto val2 = twice("hello");
+    assertEquals(val2, FILL_IN_THIS_STRING);
+  }
 }
