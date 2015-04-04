b3a87a0dc9b5bd20878e1d09b412002b041e57b0
nerdinand <nerdinand@nerdinand.com>
2015-04-04 14:07:57 +0200

    improve code styling in about_bitwise_operators.d

--- source/about_bitwise_operators.d
+++ source/about_bitwise_operators.d
@@ -5,26 +5,24 @@ import dunit;
 
 class AboutBitwiseOperators
 {
-	mixin UnitTest;
+  mixin UnitTest;
 
-	@Test
-		public void shifts()
-		{
-			auto i=1;
-            auto j=-8;
-			assertEquals(j<<3, FILL_IN_THIS_NUMBER);
-			assertEquals(j>>>3, FILL_IN_THIS_NUMBER);
-			assertEquals(j>>3, FILL_IN_THIS_NUMBER);
-			assertEquals(i<<3, FILL_IN_THIS_NUMBER);
-		}
-		
-	@Test
-	     public void operators()
-	     {
-			 assertEquals(2|4, FILL_IN_THIS_NUMBER); // or
-			 assertEquals(3&5, FILL_IN_THIS_NUMBER); // and
-			 assertEquals(6^4, FILL_IN_THIS_NUMBER); // xor
-			 assertEquals(~3, FILL_IN_THIS_NUMBER);  // not
-		 }
+  @Test
+  public void shifts() {
+    auto i = 1;
+    auto j = -8;
+    assertEquals(j << 3, FILL_IN_THIS_NUMBER);
+    assertEquals(j >>> 3, FILL_IN_THIS_NUMBER);
+    assertEquals(j >> 3, FILL_IN_THIS_NUMBER);
+    assertEquals(i << 3, FILL_IN_THIS_NUMBER);
+  }
+
+  @Test
+  public void operators() {
+    assertEquals(2 | 4, FILL_IN_THIS_NUMBER); // or
+    assertEquals(3 & 5, FILL_IN_THIS_NUMBER); // and
+    assertEquals(6 ^ 4, FILL_IN_THIS_NUMBER); // xor
+    assertEquals(~3, FILL_IN_THIS_NUMBER);  // not
+  }
 
 }
