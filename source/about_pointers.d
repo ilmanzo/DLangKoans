131efc57e78ad133f2ff9cc00638921c0a22c16a
nerdinand <nerdinand@nerdinand.com>
2015-04-04 14:56:00 +0200

    improve code styling in about_pointers.d

--- source/about_pointers.d
+++ source/about_pointers.d
@@ -3,17 +3,14 @@ module about_pointers;
 import helpers;
 import dunit;
 
-class AboutPointers
-{
-  
+class AboutPointers {
   mixin UnitTest;
+
   @Test
-  public void pointerDereference()
-  {
-    int a=3;
-    int *p=&a;
-    *p=4;
-    assertEquals(a,FILL_IN_THIS_NUMBER);
+  public void pointerDereference() {
+    int a = 3;
+    int *p = &a;
+    *p = 4;
+    assertEquals(a, FILL_IN_THIS_NUMBER);
   }
-  
 }
