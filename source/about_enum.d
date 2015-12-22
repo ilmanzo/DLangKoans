875ab1c895b1271e6608a4f406624cdbdbbc7704
Andrea Manzini <ilmanzo@gmail.com>
2015-12-22 14:20:31 +0100

    fixed error in about_enum.d

--- source/about_enum.d
+++ source/about_enum.d
@@ -6,9 +6,9 @@ import dunit;
 class AboutEnum {
   mixin UnitTest;
 
-  @Test
   enum Color { RED = 1, GREEN, BLUE };
 
+  @Test
   public void enum_properties() {
     assertEquals(Color.init, FILL_IN_THIS_NUMBER);
     assertEquals(Color.min, FILL_IN_THIS_NUMBER);
